bf53a56e3161e7217f23a9918c21f3873181cda1
Adithyan S Raj <adithyansraj20@gmail.com>
2023-04-08 22:39:37 +0530

    missing data in R and Encoding Categorical Data

--- Part 1 -  Data Preprocessing/R/data_preprocessing.r
+++ Part 1 -  Data Preprocessing/R/data_preprocessing.r
@@ -1,6 +1,25 @@
 # Data Preprocessing
 
 # Importing the Dataset
+
 dataset = read.csv('Data.csv')
 
-# Missing Data
+# Taking care of Missing Data
+
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
+
+# Encoding Categorical Data
+
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No', 'Yes'),
+                         labels = c(0, 1))
