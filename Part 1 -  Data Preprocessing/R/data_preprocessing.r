b2e5943ba26f2bd3a69f17dd061792b45d010431
Adithyan S Raj <adithyansraj20@gmail.com>
2023-04-08 22:57:24 +0530

    Split Test and Train

--- Part 1 -  Data Preprocessing/R/data_preprocessing.r
+++ Part 1 -  Data Preprocessing/R/data_preprocessing.r
@@ -24,4 +24,17 @@ dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No', 'Yes'),
                          labels = c(0, 1))
 
+# Splitting the dataset into the Training set and Test set
+
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+
+
+
+
 
