b9fa89564030cad894b0087b5154b9f5b1e359cb
Adithyan S Raj <adithyansraj20@gmail.com>
2023-04-08 23:10:59 +0530

    feature scaling in R

--- Part 1 -  Data Preprocessing/R/data_preprocessing.r
+++ Part 1 -  Data Preprocessing/R/data_preprocessing.r
@@ -34,7 +34,9 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 
-
+# Feature Scaling
+training_set[,2:3] = scale(training_set[,2:3])
+test_set[,2:3] = scale(test_set[,2:3])
 
 
 
